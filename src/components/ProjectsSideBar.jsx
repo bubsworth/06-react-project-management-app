fbf84b89410f786b075d918e100fd3472a7a16e1
bubsworth <bubbard96@hotmail.com>
2024-01-08 15:52:33 +0000

    Handling Project Creation & Updating the UI

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-export default function ProjectsSideBar({ onStartAddProject }) {
+export default function ProjectsSideBar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -10,8 +10,13 @@ export default function ProjectsSideBar({ onStartAddProject }) {
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul className="mt-8">
-        <li className="flex justify-between my-4">x</li>
-        <li className="flex justify-between my-4">y</li>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
       </ul>
     </aside>
   );
